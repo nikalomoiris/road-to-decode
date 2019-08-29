0f7a1ac1766fd864392034740db0713fea8df445
Nikolaos Kalomoiris <nikalomoiris@gmail.com>
2019-08-29 18:53:42 -0400

    Basic sceleton of the site using bootstrap

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const About = () => (
+    <div>
+        <h2>About Page</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+    </div>
+)
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home = () => (
+    <div>
+        <h2>Hello World</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+    </div>
+)
