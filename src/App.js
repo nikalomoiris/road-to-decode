425e8d931697df0b07fab66087be7aab37bf4cab
Nikolaos Kalomoiris <nikalomoiris@gmail.com>
2019-09-03 11:53:51 -0400

    Navigation bar done with vanilla css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -6,99 +6,79 @@ import {
   Switch
 } from 'react-router-dom';
 
-import { Home } from './components/Home';
-import { Contact } from './components/Contact';
-import { About } from './components/About';
-import { NoMatch } from './components/NoMatch';
-import { Layout } from './components/Layout';
-import { NavigationBar } from './components/NavigationBar';
-import { Jumbotron } from './components/Jumbotron';
-import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
-import styled from 'styled-components';
-
-const Styles = styled.div`
-  #logo {
-    max-width:40px;
-    height:auto;
-  }
-  .navbar-brand {
-    margin: auto;
-  }
-  .navbar {
-    background-color: #8B9492;
-  }
-`;
-
 function App() {
+  const [navOpened, setNavOpened] = useState(false);
+  const navLinks = ['Home',
+    'The Tutor',
+    'Dyslexia',
+    'The Approach',
+    'Sevices',
+    'Testimonials',
+    'Contact'];
+
+  const burger = React.createRef();
+  const sidebar = React.createRef();
+
+  const burgerClick = () => {
+    sidebar.current.classList.toggle('nav-active');
+    burger.current.classList.toggle('toggle');
+    setNavOpened(!navOpened);
+  };
+
   return (
     <React.Fragment>
-      <Styles>
-        <Navbar bg="light" light expand="md" sticky="top" className="bg-white justify-content-center">
-          <Navbar.Brand href="#" className="align-items-center">
-            <img id="logo" src="logo.png" alt="Logo" />
-          </Navbar.Brand>
-        </Navbar>
-        <Navbar bg="light" light expand="md" sticky="top" className="bg-white justify-content-center navbar-static-top navbar-lower">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-            <Nav className="align-items-center">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">The Tutor</Nav.Link>
-              <Nav.Link href="#">Dyslexia</Nav.Link>
-              <Nav.Link href="#">The Approach</Nav.Link>
-              <Nav.Link href="#">Services</Nav.Link>
-              <Nav.Link href="#">Testimonials</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Jumbotron />
-        {/* <nav className="navbar navbar-expand-md bg-light navbar-light sticky-top border-bottom border-gray bg-white">
-          <div className="d-flex">
-            <a href="#" className="navbar-brand">
-              <img id="logo" src="logo.png" alt="Logo" />
-            </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-              data-target="#collapsibleNavbar">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="collapsibleNavbar">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a href="#" className="nav-link">Home</a>
+      <nav>
+        <div className="logo">
+          <img src="Logo-gradient.png" alt="logo" />
+        </div>
+        <ul className="nav-links" ref={sidebar}>
+          {navOpened
+            ? navLinks.map((link, index) => {
+              return (
+                <li key={index}
+                  href="#"
+                  style={{
+                    animation: `navLinkFade 0.5s ease forwards ${index / 7 + 0.1}s`
+                  }}
+                >
+                  {link}
                 </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">The Tutor</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">Dyslexia</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">The Approach</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">Services</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">Testimonials</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#" className="nav-link">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav> */}
-        <Layout>
-          <Router>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-              <Route path='/contact' component={Contact} />
-            </Switch>
-          </Router>
-        </Layout>
-      </Styles>
+              );
+            })
+            : navLinks.map((link, index) => {
+              return (
+                <li key={index}
+                  href="#">{link}</li>
+              )
+            })}
+          {/* <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">The Tutor</a>
+          </li>
+          <li>
+            <a href="#">Dyslexia</a>
+          </li>
+          <li>
+            <a href="#">The Approach</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Testimonials</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li> */}
+        </ul>
+        <div className="burger" ref={burger} onClick={burgerClick}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </nav>
     </React.Fragment>
   );
 }
--- src/components/Contact.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Contact = () => (
-    <div>
-        <h2>Contact Us</h2>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
-    </div>
-)
